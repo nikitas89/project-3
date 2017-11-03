9d2f4866bac082271cc156cbf89955e0079ec3e9
nikitas89 <cvnikita1@gmail.com>
2017-11-03 15:55:33 +0800

    created Group model and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103074836) do
+ActiveRecord::Schema.define(version: 20171103075421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "email"
