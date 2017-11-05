16588e99ed6ec77945f921d4b5979072017981bc
nikitas89 <cvnikita1@gmail.com>
2017-11-05 14:43:20 +0800

    attempted migrate failed. added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  new_user = User.new
+  new_user.name = Faker::HarryPotter.character
+  new_user.email = Faker::Internet.email
+  new_user.password = 'test123'
+  new_user.save
+end
+
+Cuisine.create([{ name: 'Local' }, { name: 'Vegetarian' }, { name: 'Western' }, { name: 'Halal' }, { name: 'Thai' }])
+5.times do
+  new_group = Group.new
+  new_group.name = Faker::StarWars.specie
+  new_group.save
+end
+
+cuisines  = Cuisine.all
+5.times do
+  new_resto = Restaurant.new
+  new_resto.name = Faker::Team.name
+  new_resto.address = Faker::Address.street_address
+  new_resto.phone = Faker::PhoneNumber.cell_phone
+  new_resto.longitude =Faker::Address.longitude
+  new_resto.latitude = Faker::Address.latitude
+  new_resto.cuisine = cuisines.first
+  new_resto.save
+end
