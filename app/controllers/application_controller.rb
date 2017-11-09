dfe328d21e7490f43d97198e86c409158788afac
nikitas89 <cvnikita1@gmail.com>
2017-11-09 12:10:37 +0800

    added groups creation and name field to register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -6,12 +7,18 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :contact_number])
+
   end
 
-  #if change for account_update prefix.
-  # def configure_permitted_parameters_edit
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  #   # devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :contact_number])
-  # end
 end
+
+
+
+
+
+
+#if change for account_update prefix.
+# def configure_permitted_parameters_edit
+#   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+#   # devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :contact_number])
+# end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,12 +2,13 @@ class GroupsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!, only: [:new, :destroy, :create, :edit, :update]
   def index
-    @groups = Group.all #update to only shows group of this user.
+    # @groups = Group.all #update to only shows group of this user.
+    @groups = current_user.groups.all
     # render json: @groups
   end
 
   def show
-    @group = Group.find(:id)
+    @group = current_user.groups.find(:id)
   end
 
   def new
@@ -16,15 +17,21 @@ class GroupsController < ApplicationController
 
   def create
     current_user.groups.create(params.require(:group).permit(:name))
-    redirect_to root_path
+    # redirect_to root_path
+    redirect_to groups_path
 
   end
 
   def edit
-    @group = Group.find(:id)
+    @group = Group.find(params[:id])
   end
 
   def destroy
+    # @deleted_group = Group.find(params[:id])
+    # @deleted_group.destroy
+    Group.destroy(params[:id])
+    redirect_back fallback_location:root_path
+    # redirect_to groups_path
   end
 
   def update
