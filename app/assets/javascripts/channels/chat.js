de12149b21e1fcdd842294e4daa6f27eecc9c59a
nikitas89 <cvnikita1@gmail.com>
2017-11-12 14:23:51 +0800

    sockets for group

---
+++ app/assets/javascripts/channels/chat.js
@@ -0,0 +1,33 @@
+App.chat = App.cable.subscriptions.create("ChatChannel", {
+  connected: function() {},
+  disconnected: function() {},
+  received: function(data) {
+    // alert(data.content)
+    // console.log(data.content);
+    // console.log(data.username);
+    // alert(data.username)
+    // $('div.notifications').append(data.username + ' joined '+ data.content)
+//     var htmlDelete = `<div class="alert alert-success alert-dismissible fade show notifications" role="alert">
+//      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+//        <span aria-hidden="true">&times;</span>
+//      </button>   ${data.username} deleted: ${data.content}
+// </div>`
+//     var htmlJoin = `<div class="alert alert-success alert-dismissible fade show notifications" role="alert">
+//  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+//    <span aria-hidden="true">&times;</span>
+//  </button>   ${data.username} joined: ${data.content}
+// </div>`
+
+// data.status===1? $('div.notifications').append(htmlJoin) :""
+
+// data.status==2?$('div.notifications').append(htmlDelete): ""
+// $('div.notifications').append(htmlDelete): ""
+$('div.notifications').empty("")
+$('div.notifications').append(data.username + ' : '+ data.content)
+  }
+});
+
+// App.cable.subscriptions.create({
+//   channel: "ChatChannel",
+//   room: "Best Room"
+// });
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   root :to => "welcome#index"
+  mount ActionCable.server, at: '/cable'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
