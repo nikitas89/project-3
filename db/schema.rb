eb1b2351aafbc2187f658210a333a5c4174a3701
koozy <koozhengyu@gmail.com>
2017-11-13 10:43:24 +0800

    merge db schema

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20171108063846) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
