7c7d11caff8d0ba700aa886a71fdf2156923c10e
nikitas89 <cvnikita1@gmail.com>
2017-11-16 13:57:52 +0800

    added field validations and flash messages for login

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 20171113090848) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
